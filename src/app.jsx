ac6449ea1cdd7207f235610622dbb58532f990eb
Paarth Monga <paarthmonga28@gmail.com>
2025-01-19 18:54:18 +0530

    signup

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
 import './app.css'
 import Layout from './Layout/Layout'
 import Home from './Pages/Home'
 
 export function App() {
   return (
-    <div>
-      <Layout>
-        <Home/>
-      </Layout>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path='/auth/signup' element={<Signup />} />
+        <Route path='/auth/login' element={<Login />} />
+      </Routes>
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
-import { render } from 'preact'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import { App } from './app.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
-render(<App />, document.getElementById('app'))
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+)
